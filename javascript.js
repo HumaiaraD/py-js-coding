052c403c3de4df3e1dd61e0c8153dbe9833d1e9a
Humaiara Diea <d.humaiara@gmail.com>
2025-06-28 08:33:13 -0400

    list vs array filtering

--- javascript.js
+++ javascript.js
@@ -1,12 +1,21 @@
-function reverse(arr){
-    let verse = [];
-    for (let i = arr.length; i < arr.length - 1; i--){
-        verse.push(arr[i])
+let input = prompt("Enter values:");
+let arr = input.split(",");  
+let arr2 = [];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].trim().length > 5) {
+        arr2.push(arr[i].trim());
     }
-    return verse;
 }
 
-//or 
-function turn(arr){
-    return arr.slice().reverse()
-}
+console.log(arr2);
+
+//or
+
+let num = prompt("Enter value: ");
+let arr3 = num.split(",");
+
+let arr4 = arr3.filter(item => item.trim().length > 5);
+
+console.log(arr4);
+
--- python.py
+++ python.py
@@ -1,11 +1,6 @@
-def reverse(lst):
-    verse = []
-    for i in range(len(lst -1), -1, -1):
-        verse.append(lst[i])
-    return verse
-
-def turn(lst):
-    return lst[::-1]
-
-def turnaround(lst):
-    return list(reversed(lst))
+lst = input().split(",")
+new_list = []
+for item in lst:
+    if len(item) > 5:
+        new_list.append(item)
+print(new_list)
